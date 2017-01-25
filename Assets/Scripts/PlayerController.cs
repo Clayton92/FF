90158cd282aad4adb88e8cbb5401e33fccb28816
Clayton92 <jgclayton@outlook.com>
2017-01-25 18:09:57 +0000

    Project Setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int scrollArea;
+	public float camSpeed;
+	private float width;
+	private float height;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = true;
+		width = Screen.width;
+		height = Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Mouse Control
+		if (Input.mousePosition.x <= 0 + scrollArea) {
+			transform.Translate (Vector3.left * Time.deltaTime * camSpeed);
+		}
+		if (Input.mousePosition.x >= width - scrollArea) {
+			transform.Translate (Vector3.right * Time.deltaTime * camSpeed);
+		}
+		if (Input.mousePosition.y <= 0 + scrollArea) {
+			transform.Translate (Vector3.back * Time.deltaTime * camSpeed);
+		}
+		if (Input.mousePosition.y >= height - scrollArea) {
+			transform.Translate (Vector3.forward * Time.deltaTime * camSpeed);
+		}
+
+		//Button Control
+		if (Input.GetKey("left")) {
+			transform.Translate (Vector3.left * Time.deltaTime * camSpeed);
+		}
+		if (Input.GetKey("right")) {
+			transform.Translate (Vector3.right * Time.deltaTime * camSpeed);
+		}
+		if (Input.GetKey("down")) {
+			transform.Translate (Vector3.back * Time.deltaTime * camSpeed);
+		}
+		if (Input.GetKey("up")) {
+			transform.Translate (Vector3.forward * Time.deltaTime * camSpeed);
+		}
+		//Zoom Control
+	}
+}
